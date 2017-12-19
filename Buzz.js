b66f78737cef4fb960f0197692963f0c1c78673f
Apagyi Dávid <apagyi.david@gmail.com>
2017-12-20 00:23:25 +0100

    fuggveny

--- Buzz.js
+++
@@ -1,4 +0,0 @@
-for (var i = 1; i <= 100; i++) {
-  if (i%4==0) console.log('Buzz');
-  else console.log(i);
-}
--- Fizz.js
+++ Fizz.js
@@ -1,4 +1,12 @@
-for (var i = 1; i <= 100; i++) {
+/*for (var i = 1; i <= 100; i++) {
   if (i%3==0) console.log('Fizz');
   else console.log(i);
+}*/
+
+function buzz(a) {
+  for (var i = 1; i <= 100; i++) {
+    if (i%a==0) console.log('Fizz');
+    else console.log(i);
+  }
 }
+buzz(4);
